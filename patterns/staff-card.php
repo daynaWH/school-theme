5828b2de0792048527ce6024fd544f7250f3a904
DAYNA <dayna.cho119@gmail.com>
2025-03-24 08:44:30 -0400

    Added non-synced Staff Card pattern

---
+++ patterns/staff-card.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Staff Card
+ * Slug: school-theme/staff-card
+ * Categories: media, text, Team
+ */
+?>
+
+<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"blockGap":"0","padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"0","right":"0"}}},"backgroundColor":"primary","textColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-base-color has-primary-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--40);padding-right:0;padding-bottom:var(--wp--preset--spacing--40);padding-left:0"><!-- wp:post-featured-image {"aspectRatio":"auto","width":"300px","height":"300px","sizeSlug":"full","align":"center","style":{"border":{"radius":"100%"},"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}}} /-->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60","top":"0","bottom":"0"},"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);padding-top:0;padding-right:var(--wp--preset--spacing--60);padding-bottom:0;padding-left:var(--wp--preset--spacing--60)"><!-- wp:post-title {"textAlign":"left","level":4,"style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"0","bottom":"0","left":"0"}}}} /-->
+
+<!-- wp:post-content {"style":{"spacing":{"blockGap":"var:preset|spacing|20","padding":{"right":"0","left":"0","top":"0","bottom":"0"}}},"layout":{"type":"default"}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
