a11dbb0728b1dc817ab3a75db9eb954e947f4c6c
DAYNA <dayna.cho119@gmail.com>
2025-03-26 01:34:40 -0400

    Updated new image sizes and created pattern for Student Cards. Added styling to Students page.

--- functions.php
+++ functions.php
@@ -1,24 +1,34 @@
 <?php
 
+add_action('wp_enqueue_scripts', function () {
+    // Load normalize.css
+    wp_enqueue_style('normalize', get_theme_file_uri('/assets/css/normalize.css'), [], null);
+	// Load style.css on the front-end
+    wp_enqueue_style('school-style', get_stylesheet_uri());
+});
+
+add_action('enqueue_block_editor_assets', function () {
+    wp_enqueue_style('school-editor-style', get_stylesheet_uri());
+});
 
 function school_setup() {
     // Load style.css on the back-end editor
 	add_editor_style( get_stylesheet_uri() );
 
     // Add image sizes
-    // Crop images to 400px by 200px
-    add_image_size( '400x200', 400, 200, true );
+    // Crop images to 200px by 250px
+    add_image_size( '200X250', 200, 250, true );
 
-    // Crop images to 800px by 400px
-    add_image_size( '800x400', 800, 400, true );
+    // Crop images to 400px by 500px
+    add_image_size( '400X500', 400, 500, true );
 }
 add_action( 'after_setup_theme', 'school_setup' );
 
 // Add image sizes to the back-end
 function school_add_custom_image_sizes( $size_names ) {
 	$new_sizes = array(
-        '400x200' => esc_html__( '400x200', 'school-theme' ),
-		'800x400' => esc_html__( '800x400', 'school-theme' ),
+        '200X250' => esc_html__( '200X250', 'school-theme' ),
+		'400X500' => esc_html__( '400X500', 'school-theme' ),
 	);
 	return array_merge( $size_names, $new_sizes );
 }
@@ -37,12 +47,3 @@ function change_title_text( $title ){
     return $title;
 }
 add_filter( 'enter_title_here', 'change_title_text' );
-
-add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('normalize', get_theme_file_uri('/assets/css/normalize.css'), [], null);
-    wp_enqueue_style('school-style', get_stylesheet_uri());
-});
-
-add_action('enqueue_block_editor_assets', function () {
-    wp_enqueue_style('school-editor-style', get_stylesheet_uri());
-});
